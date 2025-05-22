5efed7b1fa70d25e682758e39d0fb73db60aa1eb
Marcelo Vega <mavegaf@gmail.com>
2025-05-22 03:07:21 -0400

    Calling endpoint api.sunrisesunset.io

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem "kamal", require: false
 # Add HTTP asset caching/compression and X-Sendfile acceleration to Puma [https://github.com/basecamp/thruster/]
 gem "thruster", require: false
 
+# HTTP client library abstraction layer https://lostisland.github.io
+gem "faraday"
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin Ajax possible
 # gem "rack-cors"
 
--- app/controllers/sunsets_controller.rb
+++ app/controllers/sunsets_controller.rb
@@ -1,5 +1,31 @@
 class SunsetsController < ApplicationController
   def index
-    render json: { error: "Error al obtener datos" }, status: :bad_gateway
+    # lat=38.907192&lng=-77.036873
+    lat = params.require(:lat)
+    lng = params.require(:lng)
+    date_start = params.require(:date_start)
+    date_end = params.require(:date_end)
+
+    conn = Faraday.new(
+      url: "https://api.sunrisesunset.io",
+      params: { param: "1" },
+      headers: { "Content-Type" => "application/json" }
+    )
+
+    response = conn.get("/json") do |req|
+      req.params["lat"] = lat
+      req.params["lng"] = lng
+      req.params["date_start"] = date_start
+      req.params["date_end"] = date_end
+    end
+
+    if response.success?
+      render json: JSON.parse(response.body), status: :ok
+    else
+      render json: { error: "Failing to get data" }, status: :bad_gateway
+    end
+
+  rescue ActionController::ParameterMissing => e
+      render json: { error: e.message }, status: :bad_request
   end
 end
